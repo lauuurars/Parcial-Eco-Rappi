ef7e355b740af3e182dc01331305cf5e5c287dc7
lauuurars <1120098177@u.icesi.edu.co>
2026-04-05 14:41:52 -0500

    fix: api base url and production config

--- backend/index.js
+++ backend/index.js
@@ -2,53 +2,61 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import path from "path"
+import path from "path";
 import cors from "cors";
+import { fileURLToPath } from "url";
 
 import OrdersRouter from "./routes/modules/orders/orders.router.js";
 import UsersRouter from "./routes/modules/users/users.router.js";
 import StoresRouter from "./routes/modules/stores/stores.router.js";
 import AuthRouter from "./routes/modules/auth/auth.router.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// puerto dinámico para Render
 const PORT = process.env.PORT || 8080;
-const __dirname = import.meta.dirname
 
 const app = express();
 
+// Paths
 const frontendDistPath = path.join(__dirname, "../frontend/dist");
 const publicPath = path.join(__dirname, "public");
 
 app.use(cors());
 app.use(express.json());
 
+//Rutas API
 app.use("/orders", OrdersRouter);
+app.use("/users", UsersRouter);
+app.use("/stores", StoresRouter);
+app.use("/api/auth", AuthRouter);
 
-app.use("/users", UsersRouter)
+// servir frontend compilado
+app.use("/rappi", express.static(frontendDistPath));
 
-app.use("/stores", StoresRouter)
+// servir landing 
+app.use("/", express.static(publicPath));
 
-app.use("/api/auth", AuthRouter)
-
-app.use("/rappi", express.static(frontendDistPath))
-app.use("/", express.static(publicPath))
-
+// SPA fallback
 app.get(/^\/rappi(\/.*)?$/, (req, res) => {
     res.sendFile(path.join(frontendDistPath, "index.html"));
-}); // frontend compilado se sirve desde express bajo la ruta /rappi desde BrowserRouter
+});
 
-app.get(/.*/, (req, res) => {
+// 
+app.get("*", (req, res) => {
     if (
         req.path.startsWith("/orders") ||
         req.path.startsWith("/users") ||
         req.path.startsWith("/stores") ||
         req.path.startsWith("/api")
     ) {
-        res.status(404).send({ message: "Not found" });
-        return;
+        return res.status(404).json({ message: "Not found" });
     }
 
     res.sendFile(path.join(publicPath, "index.html"));
 });
+
 
 app.listen(PORT, () => {
     console.log("listening on port:", PORT);
